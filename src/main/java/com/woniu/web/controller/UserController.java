b7d0d1ef94ad41fef96a8dfe63fcd7acf363ca3a
rudy <16509200@qq.com>
2019-11-07 20:23:23 +0800

    更新

--- src/main/java/com/woniu/web/controller/UserController.java
+++ src/main/java/com/woniu/web/controller/UserController.java
@@ -2,21 +2,24 @@ package com.woniu.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.woniu.domain.Users;
 import com.woniu.service.UserServiceImpl;
-
 
 @RestController
 @RequestMapping("users")
 public class UserController {
 	@Autowired
 	private UserServiceImpl us;
-	
-	  @GetMapping
-	   	public void test(){
-		  System.out.println("test");
-   	}
-	
+
+	@PostMapping
+	public void test(@RequestBody Users user) {
+		us.save(user);
+	}
+
 }
